18398ae7d84dc2dd56d636ac1f5e68262179373c
Rob Yeomans <robyeomans1@gmail.com>
2022-04-02 17:44:02 +0100

    Revert "first commit"
    This reverts commit ad9f47a3235331c5c276011e6b023dc025e76cbc.

--- sgForum/app/helpers/url_helper.php
+++
@@ -1,5 +0,0 @@
-<?php
-// simple page redirect
-function redirect($page){
-    header('location: ' . URLROOT . '/' . $page);
-}
--- sgForum/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../app/bootstrap.php';
-// $timezone = "Europe/London";
-// date_default_timezone_set($timezone);
-
-// init core library
-$init = new Core;
