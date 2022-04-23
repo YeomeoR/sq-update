98f52c47194b5c810c2e5e93c44a59612e856f9f
Rob Yeomans <robyeomans1@gmail.com>
2022-04-23 22:14:15 +0100

    Translate vanilla JS that works just fine, to jQuery for self flagilation (modal)

--- services.js
+++ services.js
@@ -3,16 +3,20 @@ $(document).ready(function () {
   $('.service-container').on('mouseenter', function () {
     $(this)
       .addClass('active')
-      .css({ opacity: '1', 'background-color': 'limegreen', 'z-index': '100' })
+      .css({
+        opacity: '1',
+        'background-color': 'rgba(189, 185, 237)',
+        'z-index': '100',
+      })
       .find('.service-details')
-      .show('p');
+      .show('slow');
   });
   // hide card text on mouseleave, remove z-index, change bg-colur to indicate 'already visited' this card, remove class active to reset styling
   $('.service-container').on('mouseleave', function () {
     $(this)
       .removeClass('active')
-      .css({ 'z-index': '1', 'background-color': '#ccc' })
+      .css({ 'z-index': '1', 'background-color': 'rgba(189, 185, 237)' })
       .find('.service-details')
-      .hide('p');
+      .hide('slow');
   });
 }); // end document ready
